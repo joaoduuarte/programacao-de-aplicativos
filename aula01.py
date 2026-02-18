126881b40d8afb08330d240ab0165d9640a811ce
joao pedro <joao.almeida01097580@sesisenaipr.otg.br>
2026-02-18 12:49:15 -0300

    primeira aula de programação

---
+++ aula01.py
@@ -0,0 +1,10 @@
+numero = 250 # variavel int
+numero_quebrado =  2.56 # variavel float 
+joao_ta_dormindo = True # variavel booleana 
+texto "aula inicial de programação" # variavel de texto 
+
+
+print(joao_ta_dormindo)
+print(texto)
+print(numero)
+print(numero_quebrado)
---
+++ aula02.py
@@ -0,0 +1,10 @@
+numero = 580 # variavel int
+numero_quebrado = 3.58 # variavel float 
+joao_lindo = True # variavel boolena 
+texto = "romeu e julieta" # variavel de texto
+
+
+print(joao_lindo)
+print(texto)
+print(numero)
+print(numero_quebrado)
