959a0a259e6d898508d064bdf4a7a930706ad37d
joaoduuarte <joao.almeida01097580@sesisenaipr.org.br>
2026-02-19 10:04:06 -0300

    atividade animal

---
+++ animal.py
@@ -0,0 +1,10 @@
+nome = "gato" # varieavel de tetxo 
+idade = 4 # variavel int
+peso = 3.58 # variravel float
+sexo = "Masculino" # varialvel de texto
+
+
+print(nome)
+print(idade)
+print(peso)
+print(sexo)
