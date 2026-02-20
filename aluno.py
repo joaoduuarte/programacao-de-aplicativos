b4c4e51853d530e3d6f5e692c3c8f6299278b0b0
joaoduuarte <joao.almeida01097580@sesisenaipr.org.br>
2026-02-20 10:02:11 -0300

    atividadematematica

---
+++ aluno.py
@@ -0,0 +1,11 @@
+nome = input ("digite seu nome :")
+print (" ola" ,nome)
+telefone = int(input("digite seu telefone: "))
+print ("seu numero é:", telefone)
+idade = int(input("digite sua idade:"))
+print ("sua idade é:",idade)
+cidade = input (" onde voce mora:")
+print (" voce mora em:", cidade)
+
+
+print("digite seu nome:")
---
+++ matematica.py
@@ -0,0 +1,17 @@
+nome = input ("digite seu nome:")
+print ("seu nome é:", nome)
+
+valor1 = int(input("digite o valor1: "))
+valor2 = int(input("digite o valor2: "))
+
+
+soma = valor1 + valor2
+subtracao = valor1 - valor2
+multiplicacao = valor1 * valor2
+divisao = valor1 / valor2
+
+
+print("A soma é", soma)
+print("A subtracao é", subtracao)
+print("A multiplicacao é", multiplicacao)
+print("A divisão é", divisao)
