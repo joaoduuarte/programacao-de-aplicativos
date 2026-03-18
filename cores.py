a100aa28cdbf3e276eb2a06664fba725199962f8
joaoduuarte <joao.almeida01097580@sesisenaipr.org.br>
2026-03-18 12:36:32 -0300

    aula18

---
+++ cores.py
@@ -0,0 +1,3 @@
+cores = ["vermelho","verde", "azul","amarelo","preto"]
+escolha = int(input("escolha um numero de o a 4 "))
+print (f"sua cor é {cores[escolha]}")
---
+++ filmes.py
@@ -0,0 +1,3 @@
+filmes = ["diario de uma paixao","batman","gente grande",]
+print(f"primeiro filme:{filmes[0]}")
+print(f"ultimo filme: {filmes[4]}")
